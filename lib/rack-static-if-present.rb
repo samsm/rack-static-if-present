c4d8be6f62a6d5d789472a8664b6cb60dc188235
Sam Schenkman-Moore <samsm@samsm.com>
2009-12-15 20:38:23 -0500

    The code, as it were.

---
+++ lib/rack-static-if-present.rb
@@ -0,0 +1,21 @@
+class Rack::StaticIfPresent
+ 
+  def initialize(app, options={})
+    @app = app
+    @urls = options[:urls] || ["/favicon.ico"]
+    root = options[:root] || Dir.pwd
+    @file_server = Rack::File.new(root)
+  end
+ 
+  def call(env)
+    path = env["PATH_INFO"]
+    can_serve = @urls.any? { |url| path.index(url) == 0 }
+ 
+    if can_serve
+      result = @file_server.call(env)
+      return result if result[0] == 200
+    end
+    @app.call(env)
+  end
+ 
+end
